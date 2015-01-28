1c4a3489665cc11647ac5c641f5256eff6edf644
waterstrider <waterstrider.vin@gmail.com>
2015-01-28 14:18:39 +0700

    changes

--- uDestination/uDestination.py
+++ uDestination/uDestination.py
@@ -8,21 +8,17 @@ class UDestination(QWidget):
         QWidget.__init__(self, parent)
 
         self.gridLayout = QGridLayout(self)
-        self.lineEdit = QLineEdit(self)
+        self.destinationPath = QLineEdit(self)
+        self.gridLayout.addWidget(self.destinationPath, 1, 2, 1, 1)
 
-        self.gridLayout.addWidget(self.lineEdit, 1, 2, 1, 1)
+        self.toFolderButton = QRadioButton("To Folder", self)
+        self.gridLayout.addWidget(self.toFolderButton, 1, 1, 1, 1)
 
-        self.radioButton_2 = QRadioButton(self)
-        self.gridLayout.addWidget(self.radioButton_2, 1, 1, 1, 1)
-
-        self.label = QLabel(self)
-
+        self.label = QLabel("Destination:", self)
         self.gridLayout.addWidget(self.label, 0, 0, 2, 1)
 
-        self.pushButton = QPushButton(self)
+        self.browseButton = QPushButton("...", self)
+        self.gridLayout.addWidget(self.browseButton, 1, 3, 1, 1)
 
-        self.gridLayout.addWidget(self.pushButton, 1, 3, 1, 1)
-
-        self.radioButton = QRadioButton(self)
-
-        self.gridLayout.addWidget(self.radioButton, 0, 1, 1, 3)
+        self.originalFolderButton = QRadioButton("To Original Folder(s)", self)
+        self.gridLayout.addWidget(self.originalFolderButton, 0, 1, 1, 3)
--- uImageViewer/uImageViwer.py
+++ uImageViewer/uImageViwer.py
@@ -18,12 +18,12 @@ class UImageViewer(QWidget):
         self.rotateRightButton = QPushButton(self)
         self.gridLayout.addWidget(self.rotateRightButton, 1, 1, 1, 1)
 
-        self.cropButton = QPushButton(self)
+        self.cropButton = QPushButton("crop", self)
         self.gridLayout.addWidget(self.cropButton, 1, 2, 1, 1)
 
-        self.effectsButton = QPushButton(self)
+        self.effectsButton = QPushButton("effect", self)
         self.gridLayout.addWidget(self.effectsButton, 1, 3, 1, 1)
 
-        self.optionsButton = QPushButton(self)
+        self.optionsButton = QPushButton("option", self)
         self.gridLayout.addWidget(self.optionsButton, 1, 4, 1, 1)
 
