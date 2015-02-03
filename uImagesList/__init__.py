c28bf694989cee129c0c4d6bf6ebc90d606e64ce
waterstrider <waterstrider.vin@gmail.com>
2015-02-03 16:01:46 +0700

    changes

--- uImagesList/__init__.py
+++ uImagesList/__init__.py
@@ -1,1 +1,14 @@
 __author__ = 'waterstrider.vin'
+
+from sys import argv
+
+from PySide.QtGui import QApplication
+
+from uImagesList.uImagesList import UImagesList
+
+
+if __name__ == "__main__":
+    app = QApplication(argv)
+    uImagesList = UImagesList()
+    uImagseList.show()
+    app.exec_()
---
+++ uImagesList/uImagesList.py
@@ -0,0 +1,27 @@
+__author__ = 'waterstrider.vin'
+
+from PySide.QtGui import *
+from PySide.QtCore import Qt
+
+from uPushButton.uPushButton import UPushButton
+
+
+class UImagesList(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        self.gridLayout = QGridLayout(self)
+        self.addButton = UPushButton(self)
+        self.gridLayout.addWidget(self.addButton, 1, 0, 1, 1)
+
+        self.removeButton = UPushButton(self)
+
+        self.gridLayout.addWidget(self.removeButton, 1, 1, 1, 1)
+
+        self.scrollArea = QScrollArea(self)
+        self.scrollArea.setWidgetResizable(True)
+
+        self.uImageList = QListWidget()
+        self.scrollArea.setWidget(self.uImageList)
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.gridLayout.addWidget(self.scrollArea, 0, 0, 1, 2)
